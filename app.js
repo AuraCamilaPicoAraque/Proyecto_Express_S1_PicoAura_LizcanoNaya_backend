b5fc034cac0af6b3a37a7e45e1057c1d0849d9d1
AuraCamilaPicoAraque <auroraprogamer@gmail.com>
2025-10-03 07:15:18 -0500

    fix: :fire: cors does not want to collaborate

--- app.js
+++ app.js
@@ -29,21 +29,16 @@ const useCredentials = String(process.env.USE_CORS_CREDENTIALS || "true").toLowe
 
 
 const options = {
-    origin: 'https://auracamilapicoaraque.github.io',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 204
+  origin: 'https://auracamilapicoaraque.github.io',
+  credentials: true,
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization','Accept'],
+  optionsSuccessStatus: 204
 };
 
 app.use(cors(options));
-app.use((req, res, next) => { if (req.method === 'OPTIONS') return res.sendStatus(204); next(); });
 
-// (Opcional) responder 204 a cualquier preflight que llegue hasta aquí
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") return res.sendStatus(204);
-  next();
-});
+app.options('*', cors(options));
 
 // Passport
 import "./config/passport.js";
